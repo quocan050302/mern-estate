efbe411d6e9e6d10bf6ee915b54787c0d60977de
Quoc An <quocan050302@gmail.com>
2023-11-27 15:50:53 +0700

    Mern estate

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0yEw6wwof3x44NPdI8rMbLb5x_PEl2ho",
+  authDomain: "mern-app-d21cd.firebaseapp.com",
+  projectId: "mern-app-d21cd",
+  storageBucket: "mern-app-d21cd.appspot.com",
+  messagingSenderId: "787058619747",
+  appId: "1:787058619747:web:3e777b17d22010dd3a4f56"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
